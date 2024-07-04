d5aec9cf6e0d475878d07eb64828b9c28de96dce
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-03 23:14:32 -0500

    Update main_routes.py

--- controladores/main_routes.py
+++ controladores/main_routes.py
@@ -8,5 +8,8 @@ def index():
 
 @main_bp.route('/api/welcome', methods=['GET'])
 def welcome_message():
-    respuesta_bot = "¡Hola! 👋 **Soy tu asistente para la reserva de servicios automotrices.** 🚗 ¿Cómo te puedo ayudar hoy? Por favor, proporcióname tu correo electrónico. 📧"
+    respuesta_bot = "¡Hola! 👋 **Soy tu asistente para la reserva de servicios automotrices.** 🚗 ¿Cómo te puedo ayudar hoy? "
     return jsonify(message=respuesta_bot)
+
+def get_welcome_message():
+    return "¡Hola! 👋 **Soy tu asistente para la reserva de servicios automotrices.** 🚗 ¿Cómo te puedo ayudar hoy? "
